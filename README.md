25a5d1e348dd8bcdb0b76dbc6d89f29445933580
simpart <simpart>
2020-12-23 03:20:36 +0000

    first

---
+++ README.md
@@ -0,0 +1,30 @@
+# mofron-comp-linktxt
+[mofron](https://mofron.github.io/mofron/) is module based frontend framework.
+
+link text component for mofron
+
+jump to url when text clicked.
+
+
+# Install
+```
+npm install mofron mofron-comp-linktxt
+```
+
+# Sample
+```html
+<setting>
+    <tag load="mofron-comp-linktxt">Link</tag>
+</setting>
+
+<Link url="https://mofron.github.io/mofron/">Link Text</Link>
+```
+
+# Parameter
+
+| Short<br>Form | Parameter Name | Type | Description |
+|:-------------:|:---------------|:-----|:------------|
+| ◯  | url | string | link url |
+| | | boolean | true: jump to url on new tab |
+| | | | false: jump to url on current tab |
+
---
+++ index.js
@@ -0,0 +1,70 @@
+/**
+ * @file mofron-comp-linktxt/index.js
+ * @brief link text component for mofron
+ *        jump to url when text clicked.
+ * @license MIT
+ */
+const Text = require("mofron-comp-text");
+const Link = require("mofron-event-link");
+
+module.exports = class extends Text {
+    /**
+     * initialize component
+     * 
+     * @param (mixed) text parameter
+     *                key-value: component config
+     * @param (string) url parameter
+     * @short text,url
+     * @type private
+     */
+    constructor (p1, p2) {
+        try {
+            super();
+            this.modname("Link");
+            this.shortForm("text", "url");
+            
+	    /* set config */
+	    if (0 < arguments.length) {
+                this.config(p1, p2);
+	    }
+        } catch (e) {
+            console.error(e.stack);
+            throw e;
+        }
+    }
+    
+    /**
+     * initialize dom contents
+     * 
+     * @type private
+     */
+    initDomConts () {
+        try {
+	    super.initDomConts();
+            this.event(new Link({ tag: "Link" }));
+            this.color("#0073bb");
+        } catch (e) {
+            console.error(e.stack);
+            throw e;
+        }
+    }
+    
+    /**
+     * link url
+     *
+     * @param (string) link url
+     * @param (boolean) true: jump to url on new tab
+     *                  false: jump to url on current tab
+     * @type parameter
+     */
+    url (prm, flg) {
+        try {
+	    let tgt = { modname: "Link", tag: "Link" };
+	    return this.event(tgt).url(prm, flg);
+	} catch (e) {
+            console.error(e.stack);
+            throw e;
+        }
+    }
+}
+/* end of file */
